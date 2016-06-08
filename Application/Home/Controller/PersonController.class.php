ff12dac7a894e7c847a1d2f55cfad8fa7ac77405
xiezhi <madder0809@163.com>
2016-06-08 14:42:00 +0800

    update 2016.6.8 PM modified by xiezhi

--- Application/Home/Controller/PersonController.class.php
+++ Application/Home/Controller/PersonController.class.php
@@ -3,15 +3,15 @@ namespace Home\Controller;
 use Think\Controller;
 class PersonController extends Controller{
 	protected function _initialize(){
-		if(!cookie("uid")){
+		if(!cookie("uid")){ //判断是否登陆
 			$this->display("log");
 			exit();
 		}
-		
 	}
 	//个人中心
 	public function index(){
-		//$info = M("user")->where
+		$info = M("user")->find(cookie("uid"));
+		$this->assign($info);
 		$this->display();
 	}
 
